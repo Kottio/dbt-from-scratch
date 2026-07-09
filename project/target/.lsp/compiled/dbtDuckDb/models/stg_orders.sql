eb6858d3a7e8c96999484a92fcd0e88b74a0bd67
kottio <thcottiaux@gmail.com>
2026-07-09 16:04:21 +0200

    Removed folders

--- project/target/.lsp/compiled/dbtDuckDb/models/stg_orders.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-id AS order_id,
-customer AS customer_id,
-ordered_at,
-store_id,
-subtotal,
-tax_paid,
-order_total
-FROM 'jaffle-data/raw_orders.csv'
--- project/target/run/dbtDuckDb/models/int_orders_done.sql
+++
@@ -1,5 +0,0 @@
-
-  
-  create view "dev"."main"."int_orders_done__dbt_tmp" as (
-    SELECT * FROM "dev"."main"."stg_orders"
-  );
